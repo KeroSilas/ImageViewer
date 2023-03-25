3b35db68263e3e9fc138c7933c24dde257051930
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2023-03-25 21:03:07 +0100

    Task and less coupling

--- src/main/java/net/kerosilas/imageviewer/ImageViewerWindowController.java
+++ src/main/java/net/kerosilas/imageviewer/ImageViewerWindowController.java
@@ -26,58 +26,49 @@ public class ImageViewerWindowController {
     @FXML private TilePane imagePane;
 
     private final List<Image> images = new ArrayList<>();
+    private final List<File> imageFiles = new ArrayList<>();
     private int currentImageIndex = 0;
-    private SlideshowRunnable slideshowRunnable;
+    private SlideshowTask slideshowTask;
 
     @FXML private void handleLoad() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select image files");
         fileChooser.getExtensionFilters().add(new ExtensionFilter("Images",
                 "*.png", "*.jpg", "*.gif", "*.tif", "*.bmp"));
-        List<File> files = fileChooser.showOpenMultipleDialog(new Stage());
-
         try {
-            if (!files.isEmpty()) {
-                files.forEach((File f) -> images.add(new Image(f.toURI().toString())));
-                displayImage(0);
-                startStopButton.setDisable(false);
-                previousButton.setDisable(false);
-                nextButton.setDisable(false);
-            }
+            imageFiles.addAll(fileChooser.showOpenMultipleDialog(new Stage()));
         } catch (NullPointerException e) {
             System.out.println("No files selected");
+        }
+
+        if (!imageFiles.isEmpty()) {
+            imageFiles.forEach((File f) -> images.add(new Image(f.toURI().toString())));
+            displayCurrentImage();
+            startStopButton.setDisable(false);
+            previousButton.setDisable(false);
+            nextButton.setDisable(false);
         }
     }
 
     @FXML private void handlePrevious() {
-        displayImage(-1);
+        stopSlideshow();
+        displayPrevImage();
     }
 
     @FXML private void handleNext() {
-        displayImage(1);
+        stopSlideshow();
+        displayNextImage();
     }
 
     @FXML private void handleStartStopSlideshow() {
-        if(slideshowRunnable.isSlideshowRunning()) {
-            slideshowRunnable.setSlideshowRunning(false);
-            startStopButton.setText("Start");
+        if (slideshowTask == null) {
+            startSlideshow();
         } else {
-            slideshowRunnable.setSlideshowRunning(true);
-            startStopButton.setText("Stop");
+            stopSlideshow();
         }
     }
 
     public void initialize() {
-        slideshowRunnable = new SlideshowRunnable((int) slideshowSpeedSlider.getValue(), false, this); // The slideshow is off by default. It will be turned on when the user clicks the start button
-        Thread slideshowThread = new Thread(slideshowRunnable);
-        slideshowThread.setDaemon(true); // This means that the thread will not prevent the application from exiting
-        slideshowThread.start();
-
-        slideshowSpeedSlider.valueProperty().addListener((observable, oldValue, newValue) -> { // Add a listener to the slider that will format the value and display it in a label
-            sliderValueLabel.setText(String.format("%ds", newValue.intValue()));
-            slideshowRunnable.setSliderValue(newValue.intValue()); // Update the slideshowRunnable object with the new slider value
-        });
-
         imagePane.setAlignment(Pos.CENTER);
 
         // Add a listener to the window size that will resize the image to fit the window
@@ -89,21 +80,66 @@ public class ImageViewerWindowController {
                 imageView.setFitWidth(newValue.doubleValue() - 16);
             });
         });
+
+        slideshowSpeedSlider.valueProperty().addListener((observable, oldValue, newValue) -> { // Add a listener to the slider that will format the value and display it in a label
+            sliderValueLabel.setText(String.format("%ds", newValue.intValue()));
+        });
     }
 
-    // This method will display the image at the currentImageIndex.
-    // indexChange can be -1, 0 or 1
-    // -1 means previous image
-    // 0 means current image
-    // 1 means next image
-    void displayImage(int indexChange) {
+    void displayCurrentImage() {
         if (!images.isEmpty()) {
-            if (currentImageIndex + indexChange < 0) {
-                currentImageIndex = images.size() - 1;
+            imageView.setImage(images.get(currentImageIndex));
+        }
+    }
+
+    void displayNextImage() {
+        if (!images.isEmpty()) {
+            if (currentImageIndex == images.size() - 1) {
+                currentImageIndex = 0;
             } else {
-                currentImageIndex = (currentImageIndex + indexChange) % images.size();
+                currentImageIndex++;
             }
             imageView.setImage(images.get(currentImageIndex));
+        }
+    }
+
+    void displayPrevImage() {
+        if (!images.isEmpty()) {
+            if (currentImageIndex == 0) {
+                currentImageIndex = images.size() - 1;
+            } else {
+                currentImageIndex--;
+            }
+            imageView.setImage(images.get(currentImageIndex));
+        }
+    }
+
+    private void startSlideshow() {
+        if (slideshowTask == null) {
+            slideshowTask = new SlideshowTask(imageFiles, (int) slideshowSpeedSlider.getValue(), currentImageIndex);
+            slideshowTask.valueProperty().addListener((ov, oldValue, newValue) -> {
+                imageView.setImage(newValue);
+            });
+            Thread thread = new Thread(slideshowTask);
+            thread.setDaemon(true);
+            thread.start();
+
+            slideshowSpeedSlider.setDisable(true);
+            startStopButton.setText("Stop");
+        }
+    }
+
+    private void stopSlideshow() {
+        if (slideshowTask != null) {
+            currentImageIndex = slideshowTask.getIndex();
+            slideshowTask.cancel();
+            slideshowTask.valueProperty().removeListener((ov, oldValue, newValue) -> {
+                imageView.setImage(newValue);
+            });
+            slideshowTask = null;
+
+            slideshowSpeedSlider.setDisable(false);
+            startStopButton.setText("Start");
         }
     }
 }
