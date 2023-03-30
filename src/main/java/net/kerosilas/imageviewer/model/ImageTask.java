35875d8c6ae7b18708b290f774a1f36b33ff9300
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2023-03-30 21:11:36 +0200

    Update ImageTask.java

--- src/main/java/net/kerosilas/imageviewer/model/ImageTask.java
+++ src/main/java/net/kerosilas/imageviewer/model/ImageTask.java
@@ -22,44 +22,38 @@ public class ImageTask extends Task<Boolean> {
 
     @Override
     public Boolean call() throws Exception {
-        ImageManager manager = ImageManager.getInstance();
         boolean success = true;
-        int count = 0;
 
         ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-        // create a list of futures to store the subtask results
         List<Future<Boolean>> futures = new ArrayList<>();
-        // divide the files list into sublists of equal size
+
         int sublistSize = (int) Math.ceil(files.size() / (double) numThreads);
         List<List<File>> sublists = new ArrayList<>();
         for (int i = 0; i < files.size(); i += sublistSize) {
             sublists.add(files.subList(i, Math.min(i + sublistSize, files.size())));
         }
-        // submit a subtask for each sublist
+
         for (List<File> sublist : sublists) {
-            futures.add(executor.submit(new SubTask(sublist, manager)));
+            futures.add(executor.submit(new SubTask(sublist)));
         }
-        // wait for all subtasks to finish and update progress
+
         for (Future<Boolean> future : futures) {
-            success = success && future.get(); // get the result of the subtask
-            count += sublistSize; // increment the count by the sublist size
-            updateProgress(count, files.size()); // update progress
+            success = success && future.get();
         }
-        // shutdown the executor
         executor.shutdown();
         return success;
     }
 }
 
-// A subtask that handles a sublist of files and adds them to the image manager
+// A subtask that handles a sublist of files and adds them to the ImageManager
 class SubTask implements Callable<Boolean> {
 
     private final List<File> files;
     private final ImageManager imageManager;
 
-    public SubTask(List<File> files, ImageManager imageManager) {
+    public SubTask(List<File> files) {
         this.files = files;
-        this.imageManager = imageManager;
+        imageManager = ImageManager.getInstance();
     }
 
     @Override
