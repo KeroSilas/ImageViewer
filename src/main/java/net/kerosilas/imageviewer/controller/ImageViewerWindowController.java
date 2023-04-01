805c1deb6ef2d5e562f9231b11629010bdd966b0
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2023-04-02 01:13:31 +0200

    Refactored some bits

--- src/main/java/net/kerosilas/imageviewer/controller/ImageViewerWindowController.java
+++ src/main/java/net/kerosilas/imageviewer/controller/ImageViewerWindowController.java
@@ -28,17 +28,18 @@ import net.kerosilas.imageviewer.model.*;
 
 public class ImageViewerWindowController {
 
-    @FXML private MFXButton startStopButton, loadButton, listButton, nextButton, previousButton, fullscreenButton;
+    @FXML private MFXButton toggleSlideshowButton, loadButton, listButton, nextButton, previousButton, fullscreenButton;
     @FXML private MFXScrollPane imageScrollPane;
     @FXML private MFXProgressSpinner progressSpinner;
     @FXML private TilePane imageTilePane;
     @FXML private Slider slideshowSpeedSlider;
     @FXML private ImageView imageView;
     @FXML private Label sliderValueLabel, nameLabel, pathLabel, blueCountLabel, greenCountLabel, redCountLabel;
-    @FXML private HBox hBoxTop, hBoxBottom, pane;
+    @FXML private HBox hBoxTop, hBoxBottom, imageHBox;
     @FXML private BorderPane root;
 
     private ImageManager imageManager;
+    private boolean isSlideshowRunning = false;
     private SlideshowTask slideshowTask;
 
     @FXML private void handleLoad() {
@@ -54,20 +55,20 @@ public class ImageViewerWindowController {
             loadButton.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #000000;");
 
             ImageTask imageTask = new ImageTask(files, 10);
-            progressSpinner.progressProperty().bind(imageTask.progressProperty());
             imageTask.setOnSucceeded(e -> {
                 for (Node node : hBoxTop.getChildren()) {
-                    node.setVisible(true);
+                    node.setDisable(false);
                     if (node instanceof MFXButton) {
                         ((MFXButton) node).setButtonType(ButtonType.RAISED);
                     }
                 }
-                imageTilePane.getChildren().setAll(imageManager.getImagePaneList());
-                updateImage();
+                loadButton.setStyle("-fx-background-color:  #30a14f; -fx-text-fill: #ffffff;");
+                root.setStyle("-fx-background-color: #131313;");
                 hBoxBottom.setVisible(true);
                 progressSpinner.setVisible(false);
-                loadButton.setDisable(false);
-                root.setStyle("-fx-background-color: #131313;");
+
+                imageTilePane.getChildren().setAll(imageManager.getImagePaneList()); // For the custom list view of images
+                updateImage();
             });
             new Thread(imageTask).start();
         }
@@ -81,12 +82,8 @@ public class ImageViewerWindowController {
         imageManager.nextImage();
     }
 
-    @FXML private void handleStartStopSlideshow() {
-        if (slideshowTask == null) {
-            startSlideshow();
-        } else {
-            stopSlideshow();
-        }
+    @FXML private void handleToggleSlideshow() {
+        toggleSlideshow();
     }
 
     @FXML private void handleFullscreen() {
@@ -104,8 +101,8 @@ public class ImageViewerWindowController {
         }
     }
 
-    @FXML private void handleImageClick(MouseEvent event) {
-        if (event.getClickCount() == 2) {
+    @FXML private void handleImageClick(MouseEvent e) {
+        if (e.getClickCount() == 2) {
             Stage stage = (Stage) root.getScene().getWindow();
             stage.setFullScreen(!stage.isFullScreen());
         }
@@ -115,30 +112,145 @@ public class ImageViewerWindowController {
         imageManager = ImageManager.getInstance();
         imageManager.addPropertyChangeListener(e -> updateImage());
 
-        Platform.runLater(() -> {
-            imageView.setFitHeight(imageView.getScene().getHeight());
-            imageView.setFitWidth(imageView.getScene().getWidth());
-            pane.setTranslateY((imageView.getScene().getHeight()) / 2);
-            pane.setTranslateX((imageView.getScene().getWidth()) / 2);
-
-            imageView.getScene().heightProperty().addListener((observable, oldValue, newValue) -> {
-                    imageView.setFitHeight((newValue.doubleValue()));
-                    pane.setTranslateY((newValue.doubleValue()) / 2);
-            });
-            imageView.getScene().widthProperty().addListener((observable, oldValue, newValue) -> {
-                    imageView.setFitWidth((newValue.doubleValue()));
-                    pane.setTranslateX((newValue.doubleValue()) / 2);
-            });
-            setupAnimations();
-        });
-        pane.toBack();
-
         slideshowSpeedSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
             sliderValueLabel.setText(String.format("%ds", newValue.intValue()));
-            if (slideshowTask != null)
+            if (isSlideshowRunning)
                 slideshowTask.setDelay(newValue.intValue());
         });
 
+        imageTilePane.setOnMouseClicked(e -> {
+            int index = imageTilePane.getChildren().indexOf(e.getTarget ());
+            if(index != -1) {
+                imageManager.setCurrentIndex(index);
+                updateImage();
+            }
+        });
+
+        Platform.runLater(() -> {
+            imageView.fitWidthProperty().bind(imageView.getScene().widthProperty());
+            imageView.fitHeightProperty().bind(imageView.getScene().heightProperty());
+            imageHBox.translateXProperty().bind(imageView.getScene().widthProperty().divide(2));
+            imageHBox.translateYProperty().bind(imageView.getScene().heightProperty().divide(2));
+
+            initializeAnimations();
+            initializeKeybindings();
+        });
+        imageHBox.toBack();
+    }
+
+    private void toggleSlideshow() {
+        if (!isSlideshowRunning) {
+            slideshowTask = new SlideshowTask((int) slideshowSpeedSlider.getValue());
+            Thread thread = new Thread(slideshowTask);
+            thread.setDaemon(true);
+            thread.start();
+
+            toggleSlideshowButton.setText("Stop slideshow");
+            toggleSlideshowButton.setStyle("-fx-background-color: #bd2323; -fx-text-fill: #ffffff;");
+            isSlideshowRunning = true;
+        } else {
+            slideshowTask.cancel();
+
+            toggleSlideshowButton.setText("Start slideshow");
+            toggleSlideshowButton.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #000000;");
+            isSlideshowRunning = false;
+        }
+    }
+
+    private void updateImage() {
+        new Thread(() -> {
+            imageView.setImage(imageManager.getCurrentImage());
+        }).start();
+        File file = imageManager.getCurrentFile();
+        Platform.runLater(() -> {
+            nameLabel.setText(file.getName().replace("%20", " "));
+            pathLabel.setText(file.getParentFile().getPath());
+            countPixelColors();
+        });
+    }
+
+    private void countPixelColors() {
+        PixelCounterTask pixelCounterTask = new PixelCounterTask();
+        pixelCounterTask.setOnSucceeded(e -> {
+            redCountLabel.setText(String.format("%d (%.2f%%)", pixelCounterTask.getValue().redCount(), pixelCounterTask.getValue().getRedPercentage() * 100));
+            greenCountLabel.setText(String.format("%d (%.2f%%)", pixelCounterTask.getValue().greenCount(), pixelCounterTask.getValue().getGreenPercentage() * 100));
+            blueCountLabel.setText(String.format("%d (%.2f%%)", pixelCounterTask.getValue().blueCount(), pixelCounterTask.getValue().getBluePercentage() * 100));
+        });
+        Thread thread = new Thread(pixelCounterTask);
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    // Animations for the top and bottom bars
+    private void initializeAnimations() {
+        TranslateTransition ttBottom = new TranslateTransition(Duration.millis(70), hBoxBottom);
+        TranslateTransition ttTop = new TranslateTransition(Duration.millis(70), hBoxTop);
+        TranslateTransition ttList = new TranslateTransition(Duration.millis(70), imageScrollPane);
+        PauseTransition pause = new PauseTransition(Duration.millis(3000));
+
+        pause.play();
+        pause.setOnFinished(e -> {
+            ttTop.setByY(-50);
+            ttBottom.setByY(50);
+            ttList.setByY(-128);
+            ttTop.play();
+            ttBottom.play();
+            ttList.play();
+
+            pause.playFromStart();
+        });
+
+        root.getScene().setOnMouseMoved(e -> {
+            if (imageScrollPane.getTranslateY() != 0) {
+                ttTop.setByY(50);
+                ttBottom.setByY(-50);
+                ttList.setByY(128);
+                ttTop.play();
+                ttBottom.play();
+                ttList.play();
+            }
+            pause.playFromStart();
+        });
+
+        root.getScene().setOnMouseEntered(e -> {
+            if (imageScrollPane.getTranslateY() != 0) {
+                ttTop.setByY(50);
+                ttBottom.setByY(-50);
+                ttList.setByY(128);
+                ttTop.play();
+                ttBottom.play();
+                ttList.play();
+            }
+            pause.playFromStart();
+        });
+
+        root.getScene().setOnMouseExited(e -> {
+            if (imageScrollPane.getTranslateY() == 0) {
+                ttTop.setByY(-50);
+                ttBottom.setByY(50);
+                ttList.setByY(-128);
+                ttTop.play();
+                ttBottom.play();
+                ttList.play();
+            }
+            pause.playFromStart();
+        });
+
+        slideshowSpeedSlider.setOnMouseDragged(e -> {
+            pause.playFromStart();
+        });
+
+        for (Node node : hBoxTop.getChildren()) {
+            node.setOnMouseEntered(event -> {
+                pause.stop();
+            });
+            node.setOnMouseExited(event -> {
+                pause.playFromStart();
+            });
+        }
+    }
+
+    private void initializeKeybindings() {
         root.addEventHandler(KeyEvent.KEY_PRESSED, e -> {
             switch (e.getCode()) {
                 case D -> {
@@ -150,7 +262,7 @@ public class ImageViewerWindowController {
                     e.consume();
                 }
                 case S -> {
-                    startStopButton.fire();
+                    toggleSlideshowButton.fire();
                     e.consume();
                 }
                 case F -> {
@@ -163,127 +275,5 @@ public class ImageViewerWindowController {
                 }
             }
         });
-
-        imageTilePane.setOnMouseClicked(event -> {
-            int index = imageTilePane.getChildren().indexOf(event.getTarget ());
-            if(index != -1) {
-                imageManager.setCurrentIndex(index);
-                updateImage();
-            }
-        });
-    }
-
-    private void startSlideshow() {
-        if (slideshowTask == null) {
-            slideshowTask = new SlideshowTask((int) slideshowSpeedSlider.getValue());
-            Thread thread = new Thread(slideshowTask);
-            thread.setDaemon(true);
-            thread.start();
-
-            startStopButton.setText("Stop slideshow");
-            startStopButton.setStyle("-fx-background-color: #bd2323; -fx-text-fill: #ffffff;");
-        }
-    }
-
-    private void stopSlideshow() {
-        if (slideshowTask != null) {
-            slideshowTask.cancel();
-            slideshowTask = null;
-
-            startStopButton.setText("Start slideshow");
-            startStopButton.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #000000;");
-        }
-    }
-
-    private void updateImage() {
-        new Thread(() -> {
-            imageView.setImage(imageManager.getCurrentImage());
-        }).start();
-        File file = imageManager.getCurrentFile();
-        Platform.runLater(() -> { // run on JavaFX thread
-            nameLabel.setText(file.getName().replace("%20", " "));
-            pathLabel.setText(file.getParentFile().getPath().substring(6));
-        });
-        countPixelColors();
-    }
-
-    private void countPixelColors() {
-        PixelCounterTask pixelCounterTask = new PixelCounterTask();
-        pixelCounterTask.valueProperty().addListener((ov, oldValue, newValue) -> {
-            redCountLabel.setText(String.format("%d (%.2f%%)", newValue.redCount(), newValue.getRedPercentage() * 100));
-            greenCountLabel.setText(String.format("%d (%.2f%%)", newValue.greenCount(), newValue.getGreenPercentage() * 100));
-            blueCountLabel.setText(String.format("%d (%.2f%%)", newValue.blueCount(), newValue.getBluePercentage() * 100));
-        });
-        Thread thread = new Thread(pixelCounterTask);
-        thread.setDaemon(true);
-        thread.start();
-    }
-
-    private void setupAnimations() {
-        TranslateTransition ttBottom = new TranslateTransition(Duration.millis(70), hBoxBottom);
-        TranslateTransition ttTop = new TranslateTransition(Duration.millis(70), hBoxTop);
-        TranslateTransition ttList = new TranslateTransition(Duration.millis(70), imageScrollPane);
-        PauseTransition pause = new PauseTransition(Duration.millis(3000));
-
-        pause.play();
-        pause.setOnFinished(event -> {
-            ttTop.setByY(-50);
-            ttBottom.setByY(50);
-            ttList.setByY(-128);
-            ttTop.play();
-            ttBottom.play();
-            ttList.play();
-
-            pause.playFromStart();
-        });
-
-        root.getScene().setOnMouseMoved(event -> {
-            if (imageScrollPane.getTranslateY() != 0) {
-                ttTop.setByY(50);
-                ttBottom.setByY(-50);
-                ttList.setByY(128);
-                ttTop.play();
-                ttBottom.play();
-                ttList.play();
-            }
-            pause.playFromStart();
-        });
-
-        root.getScene().setOnMouseEntered(event -> {
-            if (imageScrollPane.getTranslateY() != 0) {
-                ttTop.setByY(50);
-                ttBottom.setByY(-50);
-                ttList.setByY(128);
-                ttTop.play();
-                ttBottom.play();
-                ttList.play();
-            }
-            pause.playFromStart();
-        });
-
-        root.getScene().setOnMouseExited(event -> {
-            if (imageScrollPane.getTranslateY() == 0) {
-                ttTop.setByY(-50);
-                ttBottom.setByY(50);
-                ttList.setByY(-128);
-                ttTop.play();
-                ttBottom.play();
-                ttList.play();
-            }
-            pause.playFromStart();
-        });
-
-        slideshowSpeedSlider.setOnMouseDragged(event -> {
-            pause.playFromStart();
-        });
-
-        for (Node node : hBoxTop.getChildren()) {
-            node.setOnMouseEntered(event -> {
-                pause.stop();
-            });
-            node.setOnMouseExited(event -> {
-                pause.playFromStart();
-            });
-        }
     }
 }
